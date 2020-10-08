93dadc58a096cdba112b6caee0f1f5af6842f209
Stpehanie Lange <lissisa2003@gmail.com>
2020-10-07 22:21:29 -0600

    - Changed app title - Added movie card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MovieCardComponent } from './movie-card/movie-card-component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/movie-card/movie-card-component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+
+@Component({
+    selector: 'movie-card',
+    templateUrl: './movie-card.html',
+    styleUrls: ['./movie-card.scss']
+  })
+  export class MovieCardComponent {
+    constructor() { }
+
+
+
+  }
