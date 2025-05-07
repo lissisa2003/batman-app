27c59dd0a700aa9962c27b11d5efe31094d591c8
lissisa2003 <lissisa2003@gmail.com>
2025-05-07 09:58:19 -0600

    18 to 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { Movie, MovieDetail } from './movie';
 import { MovieService } from './movie.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent implements OnInit {
   title = 'batman-app';
--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -5,8 +5,9 @@ import { MovieDetail } from '../movie';
 @Component({
     selector: 'movie-card',
     templateUrl: './movie-card.html',
-    styleUrls: ['./movie-card.scss']
-  })
+    styleUrls: ['./movie-card.scss'],
+    standalone: false
+})
   export class MovieCardComponent {
     @Input() movie: MovieDetail;
 
