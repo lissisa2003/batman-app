e10e64f04e4c925341ece1912b8c6e5d75f53088
Stephanie <lissisa2003@gmail.com>
2025-05-07 12:33:06 -0600

    Update version to 19 (#1)
    * cli up
    
    * 10 to 11
    
    * 11 to 12
    
    * 12 to 13
    
    * 13 to 14
    
    * 14 to 15
    
    * Add .angular to gitignore
    
    * 15 to 16
    
    * 16 to 17
    
    * 17 to 18
    
    * 18 to 19
    
    * Change to standalone compoent structure, remove module

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,15 +1,13 @@
-import { Component, Input } from "@angular/core";
+import { Component, input } from "@angular/core";
 import { MovieDetail } from '../movie';
 
 
 @Component({
     selector: 'movie-card',
     templateUrl: './movie-card.html',
-    styleUrls: ['./movie-card.scss']
-  })
+    styleUrls: ['./movie-card.scss'],
+})
   export class MovieCardComponent {
-    @Input() movie: MovieDetail;
-
-    constructor() { }
+    movie = input<MovieDetail>();
 
   }
--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Movie, MovieDetail } from './movie';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
-  private apiUrl = 'http://www.omdbapi.com/';
-  private apiKey = '&apikey=5a488304';
+  #apiUrl = 'http://www.omdbapi.com/';
+  #apiKey = '&apikey=5a488304';
 
   constructor( private http: HttpClient) { }
 
 
   getMovieList(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}?s=Batman${this.apiKey}`);
+    return this.http.get<any>(`${this.#apiUrl}?s=Batman${this.#apiKey}`);
   }
 
   getMovieDetail(imdbId: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}?i=${imdbId}${this.apiKey}`);
+    return this.http.get<any>(`${this.#apiUrl}?i=${imdbId}${this.#apiKey}`);
   }
 }
